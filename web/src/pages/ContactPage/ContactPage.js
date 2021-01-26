2f61f6a31e5ce351af171842393ffdb01a0f02d4
Willian Krause <krause.willian@gmail.com>
2021-01-26 19:17:34 -0300

    Form

---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,62 @@
+import {
+  FieldError,
+  Form,
+  Label,
+  Submit,
+  TextAreaField,
+  TextField,
+} from '@redwoodjs/forms'
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <BlogLayout>
+      <Form onSubmit={onSubmit} validation={{ mode: 'onBlur' }}>
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name="email" errorClassName="error">
+          E-mail
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /[^@]+@[^.]+\..+/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
+        <Submit>Send</Submit>
+      </Form>
+    </BlogLayout>
+  )
+}
+
+export default ContactPage
