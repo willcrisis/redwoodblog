99631e9aec0045258f5258c36ed56c7095b6b929
Willian Krause <krause.willian@gmail.com>
2021-01-26 19:03:15 -0300

    Routing

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+import BlogPostsCell from 'src/components/BlogPostsCell'
+
+const HomePage = () => {
+  return (
+    <BlogLayout>
+      <BlogPostsCell />
+    </BlogLayout>
+  )
+}
+
+export default HomePage
